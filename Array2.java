7a2fc002eb86482382beb97eea6fe45cc0d194b9
RajiObsqura <160754853+RajiObsqura@users.noreply.github.com>
2024-02-24 13:48:15 +0530

    my second file

---
+++ Array2.java
@@ -0,0 +1,22 @@
+//program to show Array2
+
+public class Array2
+{
+public static void main(String args[])
+{
+String arr[]=new String[5];
+arr[0]="RED";
+arr[1]="BLue";
+arr[2]="PINK";
+//arr[3]="yellow";
+//arr[4]="Lavendar";
+for (int i=0;i<arr.length;i++)
+  {
+   System .out.println(arr[i]);
+  }
+
+}
+
+}
+
+
